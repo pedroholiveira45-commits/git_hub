4c3fdee10572f7054fe63b801d20dbe33bf70d6e
pedroholiveira45-commits <pedro.h.oliveira45@aluno.senai.br>
2026-03-26 15:19:06 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// index.js
+
+// Mostra uma mensagem no console
+console.log("Olá, mundo! 🚀");
+
+// Criando uma função simples
+function saudacao(nome) {
+    return "Olá, " + nome + "!";
+}
+
+// Chamando a função
+let mensagem = saudacao("Pedro");
+console.log(mensagem);
+localStorage
